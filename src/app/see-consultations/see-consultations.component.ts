718bcefcea07528048ad7603724d220d9ee38204
luismiguel44 <luism4h@gmail.com>
2024-09-10 12:27:45 -0500

    Update components

--- src/app/see-consultations/see-consultations.component.ts
+++ src/app/see-consultations/see-consultations.component.ts
@@ -1,15 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, Inject, LOCALE_ID } from '@angular/core';
 import { ConsultationsService } from '../consultations.service';
+import { FormsModule } from '@angular/forms';
+import { formatDate, NgFor } from '@angular/common';
+import { Consultation } from '../consultation';
 
 @Component({
   selector: 'app-see-consultations',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, NgFor],
   templateUrl: './see-consultations.component.html',
   styleUrl: './see-consultations.component.css'
 })
 export class SeeConsultationsComponent {
 
-  constructor(private consultationsService: ConsultationsService) { }
+  cc:string = '';
+  minDate: Date = new Date();
+  maxDate: Date = new Date();
+  consultations: Consultation[] = [];
+  formatingDate: Function = (date:Date) => formatDate(date, "dd-MM-YYYY hh:mm a", this.locale);
+
+  constructor(private consultationsService: ConsultationsService,
+    @Inject(LOCALE_ID) private locale: string) { }
+
+  buscar(){
+    if(!this.cc){
+      this.consultationsService.getConsultations(this.formatingDate(this.minDate),this.formatingDate(this.maxDate))
+      .subscribe(data => {
+        this.consultations = data;
+        this.consultations.forEach(con => con.imageUrl = this.consultationsService.getAuthorizationImage(con.consultationCode))
+      })
+    } else {
+      this.consultationsService.getConsultationsByCC(this.cc,this.formatingDate(this.minDate),this.formatingDate(this.maxDate))
+      .subscribe(data => {
+        this.consultations = data;
+        this.consultations.forEach(con => con.imageUrl = this.consultationsService.getAuthorizationImage(con.consultationCode))
+      })
+    }
+  }
 
 }
